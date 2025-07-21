0f45957c67e5f376f773144d7dde29224545d8fc
Jared Claypoole <jared.claypoole@sri.com>
2025-07-21 15:30:44 -0400

    Add k8s-test-dag2.py

---
+++ k8s-test-dag2.py
@@ -0,0 +1,15 @@
+# following https://airflow.apache.org/docs/apache-airflow-providers-cncf-kubernetes/stable/operators.html#kubernetespodoperator
+
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+
+k = KubernetesPodOperator(
+    name="hello-dry-run",
+    image="debian",
+    cmds=["bash", "-cx"],
+    arguments=["echo", "10"],
+    labels={"foo": "bar"},
+    task_id="dry_run_demo",
+    do_xcom_push=True,
+)
+
+k.dry_run()
